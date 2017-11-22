21cf74a7ce0d96830e0ec0a0f78dae67cada2b31
silvaCarlosE <ces.eduardo98@gmail.com>
2017-11-22 03:10:16 -0200

    Crud manutenções

---
+++ PrjIntegrado/Controllers/ManutencaoController.cs
@@ -0,0 +1,104 @@
+﻿using PrjIntegrado.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Web;
+using System.Web.Mvc;
+using System.Windows.Forms;
+
+namespace PrjIntegrado.Controllers
+{
+    public class ManutencaoController : Controller
+    {
+        // GET: Manutencao
+        public ActionResult Index()
+        {
+            Manutencao aux = new Manutencao();
+            List<Manutencao> list = new List<Manutencao>();
+            List<Tecnico> tecnicos = new List<Tecnico>();
+            List<Impressora> impressoras = new List<Impressora>();
+
+            list = aux.getManutencoes();
+            tecnicos = aux.GetTecnicos();
+            impressoras = aux.GetImpressoras();
+            if (tecnicos.Count == 0 || impressoras.Count == 0)
+            {
+                ViewData["errorMsg"] = "É necessário o cadastro de ao menos um técnico e uma impressora para realizar o cadastro de uma manutenção";
+            }
+            else
+            {
+                ViewData["errorMsg"] = "";
+            }
+            ViewData["tecnicos"] = tecnicos;
+            ViewData["impressoras"] = impressoras;
+            ViewBag.List = list;
+            return View(list);
+        }
+
+        [HttpPost]
+        public ActionResult Update(System.Web.Mvc.FormCollection collection)
+        {
+            Manutencao aux = new Manutencao();
+            aux = aux.selectById(int.Parse(collection[0]));
+            List<Tecnico> tecnicos = new List<Tecnico>();
+            List<Impressora> impressoras = new List<Impressora>();
+            tecnicos = aux.GetTecnicos();
+            impressoras = aux.GetImpressoras();
+
+            ViewData["impressoras"] = impressoras;
+            ViewData["tecnicos"] = tecnicos;
+            ViewData["Manutencao"] = aux;
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Delete(System.Web.Mvc.FormCollection collection)
+        {
+            int idToExclude = int.Parse(Regex.Replace(collection[0], ",", ""));
+            Manutencao aux = new Manutencao();
+            bool result;
+            result = aux.DeleteManutencao(idToExclude);
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public ActionResult Save(System.Web.Mvc.FormCollection collection)
+        {
+            Manutencao aux = new Manutencao();
+            aux.Id = int.Parse(collection[0]);
+            aux.Defeito = collection[1];
+            aux.Data = collection[2];
+            aux.Id_tecnico = int.Parse(collection[3]);
+            aux.Id_impressora = int.Parse(collection[4]);
+            aux.Valor_gasto = double.Parse(collection[5]);
+            bool result = aux.Update(aux);
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public ActionResult Insert(System.Web.Mvc.FormCollection collection)
+        {
+            Manutencao aux = new Manutencao();
+            aux.Defeito = collection[1];
+            aux.Data = collection[2];
+            aux.Id_tecnico = int.Parse(collection[3]);
+            aux.Id_impressora = int.Parse(collection[4]);
+            aux.Valor_gasto = int.Parse(collection[5]);
+            bool result;
+            result = aux.Insert(aux);
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public ActionResult Search(System.Web.Mvc.FormCollection collection)
+        {
+            string nome = collection[0];
+            Manutencao aux = new Manutencao();
+            List<Manutencao> list = new List<Manutencao>();
+            list = aux.getManutencoes(nome);
+            ViewBag.List = list;
+            return View(list);
+        }
+    }
+}
---
+++ PrjIntegrado/Models/Manutencao.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PrjIntegrado.Models
+{
+    public class Manutencao
+    {
+        public int Id { get; set; }
+        public string Defeito { get; set; }
+        public string Data { get; set; }
+        public int Id_tecnico { get; set; }
+        public int Id_impressora { get; set; }
+        public double Valor_gasto { get; set; }
+
+        public List<Manutencao> getManutencoes()
+        {
+            DbConnection dbConnection = new DbConnection();
+            List<Manutencao> manutencao = new List<Manutencao>();
+            string tableName = "manutencoes";
+            string fields = " id_manut, defeito, data_manut, id_tecnico, Id_impressora, valor_gasto ";
+            var result = dbConnection.Select(tableName, fields);
+            if (result.HasRows)
+            {
+                while (result.Read())
+                {
+                    Manutencao aux = new Manutencao();
+                    aux.Id = result.GetInt32(0);
+                    aux.Defeito = result.GetString(1);
+                    aux.Data = result.GetString(2);
+                    aux.Id_tecnico = result.GetInt32(3);
+                    aux.Id_impressora = result.GetInt32(4);
+                    aux.Valor_gasto = result.GetDouble(4);
+                    manutencao.Add(aux);
+                }
+            }
+            return manutencao;
+        }
+
+        public List<Manutencao> getManutencoes(string defeito)
+        {
+            DbConnection dbConnection = new DbConnection();
+            List<Manutencao> manutencao = new List<Manutencao>();
+            string table = "manutencoes";
+            string like = " defeito LIKE '%" + defeito + "%'";
+            var result = dbConnection.Search(table, like);
+            if (result.HasRows)
+            {
+                while (result.Read())
+                {
+                    Manutencao aux = new Manutencao();
+                    aux.Id = result.GetInt32(0);
+                    aux.Defeito = result.GetString(1);
+                    aux.Data = result.GetString(2);
+                    aux.Id_tecnico = result.GetInt32(3);
+                    aux.Id_impressora = result.GetInt32(4);
+                    aux.Valor_gasto = result.GetDouble(4);
+                    manutencao.Add(aux);
+                }
+            }
+            return manutencao;
+        }
+
+        public bool DeleteManutencao(int id)
+        {
+            DbConnection dbConnection = new DbConnection();
+            string tableName = "manutencoes";
+            string condition = " id_manut = " + (id.ToString());
+            dbConnection.Delete(tableName, condition);
+            return true;
+        }
+
+        public Manutencao selectById(int id)
+        {
+            DbConnection dbConnection = new DbConnection();
+            string table = "manutencoes";
+            string fields = " id_manut, defeito, data_manut, id_tecnico, id_impressora, valor_gasto ";
+            string id_field = "id_manut";
+            int id_manut = id;
+            var result = dbConnection.SelectById(table, fields, id_field, id_manut);
+            Manutencao aux = new Manutencao();
+            if (result.HasRows)
+            {
+                result.Read();
+                aux.Id = result.GetInt32(0);
+                aux.Defeito = result.GetString(1);
+                aux.Data = result.GetString(2);
+                aux.Id_tecnico = result.GetInt32(3);
+                aux.Id_impressora = result.GetInt32(4);
+                aux.Valor_gasto = result.GetDouble(4);
+            }
+            return aux;
+        }
+
+        internal bool Insert(Manutencao aux)
+        {
+            DbConnection dbConnection = new DbConnection();
+            string table = "manutencoes ";
+            string fields = "defeito, data_manut, id_tecnico, id_impressora, valor_gasto ";
+            string values = "'" + aux.Defeito + "'";
+                    values += ", " + "'" + aux.Data + "'";
+                    values += ", " + aux.Id_tecnico + ",";
+                    values += aux.Id_impressora + ", ";
+                    values += aux.Valor_gasto;
+            dbConnection.Insert(table, fields, values);
+            return true;
+        }
+
+        public bool Update(Manutencao aux)
+        {
+            DbConnection dbConnection = new DbConnection();
+            string table = "manutencoes";
+            string fields = "defeito = '" + aux.Defeito;
+                    fields += "', data_manut = '" + aux.Data + "'";
+                    fields += ", id_tecnico = " + aux.Id_tecnico.ToString();
+                    fields += ", id_impressora = " + aux.Id_impressora;
+                    fields += ", valor_gasto =" + aux.Valor_gasto.ToString();
+            string condition = "id_manut = " + (aux.Id.ToString());
+            dbConnection.Update(table, fields, condition);
+            return true;
+        }
+
+        public List<Tecnico> GetTecnicos()
+        {
+            DbConnection dbConnection = new DbConnection();
+            string table = "tecnicos";
+            string fields = "id_tecnico, nome";
+            var result = dbConnection.Select(table, fields);
+            List<Tecnico> tecnicos = new List<Tecnico>();
+            if (result.HasRows)
+            {
+                while (result.Read())
+                {
+                    Tecnico aux = new Tecnico();
+                    aux.Id = result.GetInt32(0);
+                    aux.Nome = result.GetString(1);
+                    tecnicos.Add(aux);
+                }
+            }
+            return tecnicos;
+        }
+
+        public List<Impressora> GetImpressoras()
+        {
+            DbConnection dbConnection = new DbConnection();
+            string table = "impressoras";
+            string fields = "id_impressora, nome";
+            var result = dbConnection.Select(table, fields);
+            List<Impressora> impressoras = new List<Impressora>();
+            if (result.HasRows)
+            {
+                while (result.Read())
+                {
+                    Impressora aux = new Impressora();
+                    aux.Id = result.GetInt32(0);
+                    aux.Nome = result.GetString(1);
+                    impressoras.Add(aux);
+                }
+            }
+            return impressoras;
+        }
+    }
+}
