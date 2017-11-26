13a7d25cca31aaddb48a08ffaa65da7d5e1ef3b6
gustavo10rc <gustavo10rc@gmail.com>
2017-11-26 03:14:52 -0200

    Adicao controller-view-model Login

---
+++ PrjIntegrado/Controllers/LoginController.cs
@@ -0,0 +1,46 @@
+﻿using PrjIntegrado.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Web;
+using System.Web.Mvc;
+using System.Windows.Forms;
+
+
+namespace PrjIntegrado.Controllers
+{
+    public class LoginController : Controller
+    {
+
+        // GET: Manutencao
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET:Login
+        [HttpPost]
+        public ActionResult VerificaUsuarios(System.Web.Mvc.FormCollection collection)
+        {
+            String UserName = (collection[0]);
+            String Senha = (collection[1]);
+            Login aux = new Login();
+            bool result;
+            result = aux.VerificaUsuarios(UserName, Senha);
+
+
+            if (result == false)
+            {
+                MessageBox.Show("Acesso negado. Por favor verifique seu Login e senha");
+                return RedirectToAction("Index", "Login");
+
+            }
+            else
+            {
+                Session["UsersOnline"] = UserName;
+                return RedirectToAction("Index", "RankingPerda", new { UserName = UserName });
+            }
+        }
+    }
+}
---
+++ PrjIntegrado/Models/Login.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PrjIntegrado.Models
+{
+    public class Login
+    {
+        public int IdFuncionario { get; set; }
+        public String UserName { get; set; }
+        public String Senha { get; set; }
+
+
+        internal bool VerificaUsuarios(String UserName, String Senha)
+        {
+            DbConnection dbConnection = new DbConnection();
+            string stmt = " SELECT username, senha FROM usuarios where username=" + "'" + (UserName) + "'" + " and senha = " + "'" + (Senha) + "'";
+            var result = dbConnection.GenericQuery(stmt);
+
+            Login aux = new Login();
+            if (result.HasRows)
+            {
+                while (result.Read())
+                {
+
+                    aux.UserName = result.GetString(0);
+                    aux.Senha = result.GetString(1);
+
+                }
+
+            }
+            if (aux.UserName == UserName && aux.Senha == Senha)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
+
+        public List<Funcionario> GetFuncionario()
+        {
+            DbConnection dbConnection = new DbConnection();
+            string table = "funcionarios";
+            string fields = "id_func, nome";
+            var result = dbConnection.Select(table, fields);
+            List<Funcionario> Funcionario = new List<Funcionario>();
+            if (result.HasRows)
+            {
+                while (result.Read())
+                {
+                    Funcionario aux = new Funcionario();
+                    aux.FuncionarioID = result.GetInt32(0);
+                    aux.Nome = result.GetString(1);
+                    Funcionario.Add(aux);
+                }
+            }
+            return Funcionario;
+        }
+
+
+    }
+}
